f04ae96b61d3a73a1f2efd875a2cc042574eb4e5
isabellaamandio <isabella_amandio@estudante.sesisenai.org.br>
2026-09-15 15:20:29 -0300

    Atividade 2

--- src/services/equipamento.services.js
+++ src/services/equipamento.services.js
@@ -5,6 +5,19 @@ class EquipamentoService {
         const res = await pool.query("SELECT * FROM equipamento")
         return res.rows
     }
+    async buscarPorId(id) { 
+        const res = await pool.query( "SELECT * FROM equipamentos WHERE id = $1", [id] );
+         return res.rows[0] || null; 
+    }
+    async cadastrar(nome, categoria, condicao_uso, disponibilidade){
+         const res = await pool.query( `INSERT INTO equipamentos (nome, categoria, condicao_uso, disponibilidade) 
+         VALUES ($1, $2, $3, $4) 
+         RETURNING *`, [nome, categoria, condicao_uso, disponibilidade] );
+         return res.rows[0]; 
+    }
+    async atualizarDisponibilidade(id, disponibilidade) {
+         const res = await pool.query( `UPDATE equipamentos SET disponibilidade = $1 WHERE id = $2 RETURNING *`, [disponibilidade, id] );
+          return res.rows[0] || null; }
 }
 
 export const equipamentoService = new EquipamentoService()
